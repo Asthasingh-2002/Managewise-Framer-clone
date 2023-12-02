a53ef408ae4d5b6d3ef184c8a0fd0d9106af9157
ashutoshrana <ashutosh1102.edu.in>
2023-12-02 14:41:24 +0530

    Added some componets and CSS

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,6 +2,12 @@ import React from "react";
 import img1 from "../assets/img1.webp";
 // import  '../index.css'
 import img2 from "../assets/hii_icon.webp";
+import img3 from "../assets/fireImg.webp";
+import img4 from "../assets/star.webp";
+import img5 from "../assets/smartTask.webp";
+import img6 from "../assets/flexible.webp";
+import img7 from "../assets/communication.webp";
+import img8 from "../assets/running.webp";
 
 const Container = () => {
   return (
@@ -30,44 +36,67 @@ const Container = () => {
             Watch Demo
           </button>
         </div>
-        <img src={img1} style={{maxWidth:"70%"}} height="250px"></img>
+        <img src={img1} style={{ maxWidth: "70%" }} height="250px"></img>
       </div>
 
       <div>
-        <div>hii astha</div>
-        <div>
-          Mohandas Karamchand Gandhi (2 October 1869 – 30 January 1948) was an
-          Indian lawyer, anti-colonial nationalist{" "}
-        </div>
-        <div>
-          hii dev lorem36 Empower your business with Strategic insights Powerful
-          management platform designed to streamline your business operations,
-          boost productivity, and drive success
-        </div>
-        <div>
-          Boost productivity and streamline workflow with us. Enjoy our
-          intuitive interface and robust features.
-        </div>
-        <div>
-          <img src={img1} width="200px" height="200px"></img>
-          <p>
-            Smart Task Management Say goodbye to chaos with our smart task
-            management syste
-          </p>
-        </div>
-        <div>
-          <img src={img1} width="200px" height="200px"></img>
-          <p>
-            Smart Task Management Say goodbye to chaos with our smart task
-            management syste
-          </p>
-        </div>
-        <div>
-          <img src={img1} width="200px" height="200px"></img>
-          <p>
-            Smart Task Management Say goodbye to chaos with our smart task
-            management syste
-          </p>
+        <div className="sub-container-1">
+          <div>
+            <span className=" heading-box">
+              {" "}
+              <img src={img3} height="20px" width="20px"></img>
+              PREMIER FEATURES
+            </span>
+          </div>
+
+          <div className="title-box2">
+            Discover our product's{" "}
+            <span style={{ color: "rgb(254, 129, 98)" }}>Capabilities</span>
+          </div>
+          <div className="description-box">
+            Don't settle for mediocrity - embrace the future of management with
+            Manage Wise.
+          </div>
+
+          <div className="frame1">
+            <div className="star-box">
+              <img src={img4} height="40px" width="40px"></img>
+            </div>
+            Boost productivity and streamline workflow with us. Enjoy our
+            intuitive interface and robust features.
+          </div>
+
+          <div className="frame2">
+            <img src={img5} width="400px" height="400px"></img>
+
+            <h4> Smart Task Management </h4>
+            <p style={{ color: "rgb(118, 117, 117)" }}>
+              {" "}
+              Say goodbye to chaos with our smart task management system
+            </p>
+          </div>
+          <div className="frame2">
+            <img src={img6} width="400px" height="400px"></img>
+
+            <h4>Flexible Scheduling</h4>
+            <p style={{ color: "rgb(118, 117, 117)" }}>
+              Stay productive with our flexible scheduling system
+            </p>
+          </div>
+          <div className="frame2">
+            <img src={img7} width="400px" height="400px"></img>
+            <h4>Easy Communication</h4>
+            <p style={{ color: "rgb(118, 117, 117)" }}>
+              Collaborate seamlessly with your team in real-time
+            </p>
+          </div>
+          <div className="frame2">
+            <img src={img8} width="400px" height="400px"></img>
+            <h4>Analytics</h4>
+            <p style={{ color: "rgb(118, 117, 117)" }}>
+              Gain valuable insights with our advanced analytics feature
+            </p>
+          </div>
         </div>
       </div>
     </div>
